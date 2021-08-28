86c3b09a1ccd4ebeb713e91b94cca7fefb09cd04
ezapagon <ezapagon@gmail.com>
2021-08-27 21:03:49 -0500

    Fixes2

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -44,7 +44,8 @@ function xTextRefresh() {
     );
   }
   xTextRefresh();
-
+  //xText to append 
+  //Poverty
   xText
   .append("text")
   .attr("y", -26)
@@ -52,16 +53,16 @@ function xTextRefresh() {
   .attr("data-axis", "x")
   .attr("class", "aText active x")
   .text("In Poverty (%)");
-
-xText
+ //Age
+ xText
   .append("text")
   .attr("y", 0)
   .attr("data-name", "age")
   .attr("data-axis", "x")
   .attr("class", "aText inactive x")
   .text("Age (Median)");
-
-xText
+ //Income
+ xText
   .append("text")
   .attr("y", 26)
   .attr("data-name", "income")
@@ -69,14 +70,14 @@ xText
   .attr("class", "aText inactive x")
   .text("Household Income (Median)");
 
-  var leftTextX = margin + tPadLeft;
+var leftTextX = margin + tPadLeft;
 var leftTextY = (height + labelArea) / 2 - labelArea;
 
 svg.append("g").attr("class", "yText");
 
 var yText = d3.select(".yText");
 
-
+//To change the text easily 
 function yTextRefresh() {
   yText.attr(
     "transform",
@@ -113,248 +114,233 @@ yText
   .text("Lacks Healthcare (%)");
 
 
-  d3.csv("assets/data/data.csv").then(function(data) {
+d3.csv("assets/data/data.csv").then(function(data) {
     visualize(data);
   });
 
-  function visualize(theData) {
+function visualize(theData) {
   
-    var curX = "poverty";
-    var curY = "obesity";
-    var xMin;
-    var xMax;
-    var yMin;
-    var yMax;
+  var curX = "poverty";
+  var curY = "obesity";
+  var xMin;
+  var xMax;
+  var yMin;
+  var yMax;
   
-    var toolTip = d3
-      .tip()
-      .attr("class", "d3-tip")
-      .offset([40, -60])
-      .html(function(d) {
+  var toolTip = d3
+    .tip()
+    .attr("class", "d3-tip")
+    .offset([40, -60])
+    .html(function(d) {
        
-        var theX;
+      var theX;
        
-        var theState = "<div>" + d.state + "</div>";
+      var theState = "<div>" + d.state + "</div>";
         
-        var theY = "<div>" + curY + ": " + d[curY] + "%</div>";
+      var theY = "<div>" + curY + ": " + d[curY] + "%</div>";
         
-        if (curX === "poverty") {
+      if (curX === "poverty") {
        
-          theX = "<div>" + curX + ": " + d[curX] + "%</div>";
-        }
-        else {
-         
-          theX = "<div>" +
-            curX +
-            ": " +
-            parseFloat(d[curX]).toLocaleString("en") +
-            "</div>";
-        }
+        theX = "<div>" + curX + ": " + d[curX] + "%</div>";
+      }
+      else {  
+        theX = "<div>" +
+          curX +
+          ": " +
+          parseFloat(d[curX]).toLocaleString("en") +
+          "</div>";
+      }
         
-        return theState + theX + theY;
-      });
+      return theState + theX + theY;
+    });
  
-    svg.call(toolTip);
+  svg.call(toolTip);
   
-   
-    function xMinMax() {
-      xMin = d3.min(theData, function(d) {
-        return parseFloat(d[curX]) * 0.90;
-      });
-  
-      xMax = d3.max(theData, function(d) {
+  function xMinMax() {
+    xMin = d3.min(theData, function(d) {
+      return parseFloat(d[curX]) * 0.90;
+    });
+    xMax = d3.max(theData, function(d) {
         return parseFloat(d[curX]) * 1.10;
-      });
-    }
+    });
+  }
   
-    function yMinMax() {
-      yMin = d3.min(theData, function(d) {
-        return parseFloat(d[curY]) * 0.90;
+  function yMinMax() {
+    yMin = d3.min(theData, function(d) {
+      return parseFloat(d[curY]) * 0.90;
+    }); 
+
+    yMax = d3.max(theData, function(d) {
+      return parseFloat(d[curY]) * 1.10;
       });
+  }
   
-      
-      yMax = d3.max(theData, function(d) {
-        return parseFloat(d[curY]) * 1.10;
-      });
-    }
-  
-    function labelChange(axis, clickedText) {
+  function labelChange(axis, clickedText) {
       d3
         .selectAll(".aText")
         .filter("." + axis)
         .filter(".active")
         .classed("active", false)
         .classed("inactive", true);
+
       clickedText.classed("inactive", false).classed("active", true);
-    }
+  }
   
-    xMinMax();
-    yMinMax();
+  xMinMax();
+  yMinMax();
   
-    var xScale = d3
-      .scaleLinear()
-      .domain([xMin, xMax])
-      .range([margin + labelArea, width - margin]);
-    var yScale = d3
-      .scaleLinear()
-      .domain([yMin, yMax])
+  var xScale = d3
+    .scaleLinear()
+    .domain([xMin, xMax])
+    .range([margin + labelArea, width - margin]);
+  var yScale = d3
+    .scaleLinear()
+    .domain([yMin, yMax])
      
-      .range([height - margin - labelArea, margin]);
+    .range([height - margin - labelArea, margin]);
   
     
-    var xAxis = d3.axisBottom(xScale);
-    var yAxis = d3.axisLeft(yScale);
+  var xAxis = d3.axisBottom(xScale);
+  var yAxis = d3.axisLeft(yScale);
   
-   
-    function tickCount() {
-      if (width <= 500) {
-        xAxis.ticks(5);
-        yAxis.ticks(5);
+  function tickCount() {
+    if (width <= 500) {
+      xAxis.ticks(5);
+      yAxis.ticks(5);
+    }
+    else {
+      xAxis.ticks(10);
+      yAxis.ticks(10);
+    }
+  }
+  tickCount();
+  
+    
+  svg
+    .append("g")
+    .call(xAxis)
+    .attr("class", "xAxis")
+    .attr("transform", "translate(0," + (height - margin - labelArea) + ")");
+  svg
+    .append("g")
+    .call(yAxis)
+    .attr("class", "yAxis")
+    .attr("transform", "translate(" + (margin + labelArea) + ", 0)");
+  
+  var theCircles = svg.selectAll("g theCircles").data(theData).enter();
+  
+  theCircles
+    .append("circle")
+    .attr("cx", function(d) {
+      return xScale(d[curX]);
+    })
+    .attr("cy", function(d) {
+      return yScale(d[curY]);
+    })
+    .attr("r", circRadius)
+    .attr("class", function(d) {
+      return "stateCircle " + d.abbr;
+    })
+    .on("mouseover", function(d) {  
+      toolTip.show(d, this);
+        
+      d3.select(this).style("stroke", "#323232");
+    })
+    .on("mouseout", function(d) {
+        
+      toolTip.hide(d);
+      d3.select(this).style("stroke", "#e3e3e3");
+    });
+  
+    
+  theCircles
+    .append("text")
+     
+    .text(function(d) {
+        return d.abbr;
+    })
+      
+    .attr("dx", function(d) {
+      return xScale(d[curX]);
+    })
+    .attr("dy", function(d) {
+       
+      return yScale(d[curY]) + circRadius / 2.5;
+    })
+    .attr("font-size", circRadius)
+    .attr("class", "stateText")
+     
+    .on("mouseover", function(d) {
+        
+      toolTip.show(d);
+       
+      d3.select("." + d.abbr).style("stroke", "#323232");
+    })
+    .on("mouseout", function(d) {
+        
+      toolTip.hide(d);
+        
+      d3.select("." + d.abbr).style("stroke", "#e3e3e3");
+    });
+  
+  
+  d3.selectAll(".aText").on("click", function() {  
+    var self = d3.select(this);
+    if (self.classed("inactive")) {
+       
+      var axis = self.attr("data-axis");
+      var name = self.attr("data-name"); 
+      if (axis === "x") {
+          
+        curX = name;
+        xMinMax();
+  
+        xScale.domain([xMin, xMax]);
+  
+        svg.select(".xAxis").transition().duration(300).call(xAxis);
+  
+        d3.selectAll("circle").each(function() {
+            
+          d3
+            .select(this)
+            .transition()
+            .attr("cx", function(d) {
+              return xScale(d[curX]);
+            })
+            .duration(300);
+        });
+  
+        d3.selectAll(".stateText").each(function() {
+          d3
+            .select(this)
+            .transition()
+            .attr("dx", function(d) {
+              return xScale(d[curX]);
+            })
+              .duration(300);
+        });
+  
+        labelChange(axis, self);
       }
       else {
-        xAxis.ticks(10);
-        yAxis.ticks(10);
-      }
-    }
-    tickCount();
-  
-    
-    svg
-      .append("g")
-      .call(xAxis)
-      .attr("class", "xAxis")
-      .attr("transform", "translate(0," + (height - margin - labelArea) + ")");
-    svg
-      .append("g")
-      .call(yAxis)
-      .attr("class", "yAxis")
-      .attr("transform", "translate(" + (margin + labelArea) + ", 0)");
-  
-    var theCircles = svg.selectAll("g theCircles").data(theData).enter();
-  
-    theCircles
-      .append("circle")
-     
-      .attr("cx", function(d) {
-        return xScale(d[curX]);
-      })
-      .attr("cy", function(d) {
-        return yScale(d[curY]);
-      })
-      .attr("r", circRadius)
-      .attr("class", function(d) {
-        return "stateCircle " + d.abbr;
-      })
-     
-      .on("mouseover", function(d) {
         
-        toolTip.show(d, this);
-        
-        d3.select(this).style("stroke", "#323232");
-      })
-      .on("mouseout", function(d) {
-        
-        toolTip.hide(d);
-        
-        d3.select(this).style("stroke", "#e3e3e3");
-      });
+        curY = name;
   
-    
-    theCircles
-      .append("text")
-     
-      .text(function(d) {
-        return d.abbr;
-      })
-      
-      .attr("dx", function(d) {
-        return xScale(d[curX]);
-      })
-      .attr("dy", function(d) {
-       
-        return yScale(d[curY]) + circRadius / 2.5;
-      })
-      .attr("font-size", circRadius)
-      .attr("class", "stateText")
-     
-      .on("mouseover", function(d) {
-        
-        toolTip.show(d);
-       
-        d3.select("." + d.abbr).style("stroke", "#323232");
-      })
-      .on("mouseout", function(d) {
-        
-        toolTip.hide(d);
-        
-        d3.select("." + d.abbr).style("stroke", "#e3e3e3");
-      });
+        yMinMax();
   
+        yScale.domain([yMin, yMax]);
   
-    d3.selectAll(".aText").on("click", function() {
-    
-      var self = d3.select(this);
+        svg.select(".yAxis").transition().duration(300).call(yAxis);
   
-      
-      if (self.classed("inactive")) {
-       
-        var axis = self.attr("data-axis");
-        var name = self.attr("data-name");
-  
-       
-        if (axis === "x") {
-          
-          curX = name;
-  
-       
-          xMinMax();
-  
-          xScale.domain([xMin, xMax]);
-  
-          svg.select(".xAxis").transition().duration(300).call(xAxis);
-  
-          d3.selectAll("circle").each(function() {
-            
-            d3
-              .select(this)
-              .transition()
-              .attr("cx", function(d) {
-                return xScale(d[curX]);
-              })
-              .duration(300);
-          });
-  
-          d3.selectAll(".stateText").each(function() {
-            d3
-              .select(this)
-              .transition()
-              .attr("dx", function(d) {
-                return xScale(d[curX]);
-              })
-              .duration(300);
-          });
-  
-          labelChange(axis, self);
-        }
-        else {
-        
-          curY = name;
-  
-          yMinMax();
-  
-          yScale.domain([yMin, yMax]);
-  
-          svg.select(".yAxis").transition().duration(300).call(yAxis);
-  
-          d3.selectAll("circle").each(function() {
+        d3.selectAll("circle").each(function() {
            
-            d3
-              .select(this)
-              .transition()
-              .attr("cy", function(d) {
-                return yScale(d[curY]);
-              })
-              .duration(300);
+          d3
+            .select(this)
+            .transition()
+            .attr("cy", function(d) {
+              return yScale(d[curY]);
+            })
+            .duration(300);
           });
   
           d3.selectAll(".stateText").each(function() {
